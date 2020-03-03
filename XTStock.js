357655698d6e0fc42fa8754ddc6596520fded752
aaronrainbird <aaronrainbird@gmail.com>
2020-03-03 11:47:25 +0000

    Delete XTStock.js

--- XTStock.js
+++
@@ -1,9 +0,0 @@
-var rows = document.getElementsByClassName('wide-data')[3].children[1].children
-for (var a = 0;a<rows.length-1;a++) {
-for (var b = 5;b<9;b++) {
-var stockValue = rows[a].children[b]
-if (stockValue.innerText == 0) {
-stockValue.outerHTML = stockValue.outerHTML.replace("<td","<td style='color:lightgrey;'")
-} 
-}
-}
